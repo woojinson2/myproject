037b4aac4711b14d7e49875090ddf607822eedb2
woojinson2 <woojinson0525@gmail.com>
2024-10-26 09:21:47 +0900

    Add files via upload

---
+++ project.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import sqlite3
+
+pages = {
+    "카테고리1" : [
+        st.Page("page/a.py", title="회원가입"),
+        st.Page("page/b.py", title="로그인")
+    ],
+    "카테고리2" : [
+        st.Page("page/c.py", title="먹거리주문"),
+        st.Page("page/d.py", title="게임")
+    ]
+}
+
+pg = st.navigation(pages)
+pg.run()
+
